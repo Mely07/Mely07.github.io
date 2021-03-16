2e33ce67854b80332d8d9250069b93268e4a99b1
Mely07 <damely.tineo07@gmail.com>
2021-03-15 20:57:07 -0400

    Code cleanup

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,24 +1,14 @@
 import React, { Component } from 'react';
-import NavBar from './Navbar';
 import Home from './Home';
-import Projects from './Projects';
-import Contact from './Contact';
-import Blog from './Blog'
-
-
 import { BrowserRouter as Switch, Route } from 'react-router-dom';
+
 
 class Router extends Component {
     render() {
         return (
             <div >
-                {/* <NavBar /> */}
-
                 <Switch>
                     <Route exact path='/' component={Home} />
-                    <Route path='/projects' component={Projects} />
-                    <Route path='/contact' component={Contact} />
-                    <Route path='/blog' component={Blog} />
                 </Switch>
             </div>
         );
